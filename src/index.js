0a43235cbf7268645b14de22b6e96798863c2374
Meqdad-alhashmi <98832003+Meqdad-alhashmi@users.noreply.github.com>
2022-05-24 22:24:58 +0200

    React 18 II

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { createRoot } from "react-dom/client";
-import React from "react";
+import { StrictMode } from "react";
 import App from "./components/App";
 
-const container = document.getElementById("main");
-const root = createRoot(container);
-root.render(<App />);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
